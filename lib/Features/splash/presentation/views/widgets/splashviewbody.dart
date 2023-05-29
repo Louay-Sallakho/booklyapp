a9110cfa85db69a61d7dde526ab3b6b96a4e61cb
Louay Sallakho <louay.1997.sla@gmail.com>
2023-05-29 23:47:44 +0300

    Changed Theme & Added text to splash view

--- lib/Features/splash/presentation/views/widgets/splashviewbody.dart
+++ lib/Features/splash/presentation/views/widgets/splashviewbody.dart
@@ -10,9 +10,15 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SizedBox(
-          child: Image.asset(AssetsData.logo),
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
         ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        )
       ],
     );
   }
