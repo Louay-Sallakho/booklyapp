97378cb1d509f20868aa7f58647afd5a5b102136
Louay Sallakho <louay.1997.sla@gmail.com>
2023-06-02 00:40:26 +0300

    Added Sliding Animation to SplashView

--- lib/Features/splash/presentation/views/splashview.dart
+++ lib/Features/splash/presentation/views/splashview.dart
@@ -1,6 +1,6 @@
 
 
-import 'package:booklyapp/Features/splash/presentation/views/widgets/splashviewbody.dart';
+import 'package:booklyapp/Features/splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
---
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,46 @@
+import 'package:booklyapp/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _slidingAnimation;
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _slidingAnimation = Tween(begin: const Offset(0, 4), end: Offset.zero)
+        .animate(_animationController);
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        SlidingText(slidingAnimation: _slidingAnimation)
+      ],
+    );
+  }
+}
