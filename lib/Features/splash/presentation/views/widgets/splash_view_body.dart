d051ef0e7b57443ec40b3d1179e1e50a6a11d24d
Louay Sallakho <louay.1997.sla@gmail.com>
2023-06-02 22:12:43 +0300

    Refactor InitState & Navigate to Home with Animate

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:booklyapp/Features/Home/Presentation/Views/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:booklyapp/constants.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -15,12 +18,10 @@ class _SplashViewBodyState extends State<SplashViewBody>
   late Animation<Offset> _slidingAnimation;
   @override
   void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    _slidingAnimation = Tween(begin: const Offset(0, 4), end: Offset.zero)
-        .animate(_animationController);
-    _animationController.forward();
     super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
   }
 
   @override
@@ -42,5 +43,20 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(slidingAnimation: _slidingAnimation)
       ],
     );
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _slidingAnimation = Tween(begin: const Offset(0, 4), end: Offset.zero)
+        .animate(_animationController);
+    _animationController.forward();
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryCoor = Color(0xff100B20);
+const Duration kTransitionDuration = Duration(microseconds: 250);
