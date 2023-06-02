6f878bac3066c46fa0b6cb9fbc8aaeef879b53cc
Louay Sallakho <louay.1997.sla@gmail.com>
2023-06-02 22:35:24 +0300

    Added Custom App Bar to Home

--- lib/Features/Home/Presentation/Views/home_view.dart
+++ lib/Features/Home/Presentation/Views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/Features/Home/Presentation/Views/widgets/home_view_Body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -7,6 +8,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
