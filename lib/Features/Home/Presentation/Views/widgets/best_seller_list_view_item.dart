a6bdc3af0ea8110e375866c2ce8c59715a8a57d5
Louay Sallakho <louay.1997.sla@gmail.com>
2023-06-29 23:42:19 +0300

    Created CustomBookDetailsApp Bar

--- lib/Features/Home/Presentation/Views/widgets/best_seller_list_view_item.dart
+++ lib/Features/Home/Presentation/Views/widgets/best_seller_list_view_item.dart
@@ -1,6 +1,7 @@
+import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 import 'book_rating.dart';
@@ -10,53 +11,58 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-        child: Row(
-          children: [
-            const BestSellerLeftPic(),
-            const SizedBox(
-              width: 30,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .5,
-                    child: const Text(
-                      "harry potter and the sorcerer's stone",
-                      style: Styles.textStyle20,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  const Text(
-                    'J.K Rowling',
-                    style: Styles.textStyle14,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '19.99 €',
-                        style: Styles.textStyle20,
-                      ),
-                      BookRating(),
-                    ],
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Row(
+            children: [
+              const BestSellerLeftPic(),
+              const SizedBox(
+                width: 30,
               ),
-            )
-          ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .5,
+                      child: const Text(
+                        "harry potter and the sorcerer's stone",
+                        style: Styles.textStyle20,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    const Text(
+                      'J.K Rowling',
+                      style: Styles.textStyle14,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '19.99 €',
+                          style: Styles.textStyle20,
+                        ),
+                        BookRating(),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
+import 'package:booklyapp/Features/Home/Presentation/Views/book_details_view.dart';
 import 'package:booklyapp/Features/Home/Presentation/Views/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/views/splashview.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const String kHomeView = '/homeView';
+  static const String kBookDetailsView = '/bookDetailsView';
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +15,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+       GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
       )
     ],
   );
