3adae5898a0a5c85a982e8b2846d3453ad5703fb
Louay Sallakho <louay.1997.sla@gmail.com>
2023-06-29 22:47:43 +0300

    Created BookDetailsView

---
+++ lib/Features/Home/Presentation/Views/book_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:booklyapp/Features/Home/Presentation/Views/widgets/book_details_veiw_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BookDetailsViewBody();
+  }
+}
