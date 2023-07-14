64dd3815acaf442bcc69c8bc188c0c7f88cd6141
Louay Sallakho <louay.1997.sla@gmail.com>
2023-07-14 23:11:07 +0300

    Added titel and subtitle to details view

--- lib/Features/Home/Presentation/Views/widgets/book_details_veiw_body.dart
+++ lib/Features/Home/Presentation/Views/widgets/book_details_veiw_body.dart
@@ -1,4 +1,5 @@
 import 'package:booklyapp/Features/Home/Presentation/Views/widgets/custom_book_image.dart';
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_app_bar.dart';
@@ -13,12 +14,29 @@ class BookDetailsViewBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const CustomeBookDetailsAppBar(),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: movieItemPadding * .17),
               child: const CustomBookImage(),
             ),
+            const SizedBox(
+              height: 34,
+            ),
+            const Text(
+              'Harry Botter and the Sorecerrey Stone',
+              style: Styles.textStyle20,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              'JK. Rowling',
+              style: Styles.textStyle18
+                  .copyWith(color: Colors.white.withOpacity(.4)),
+            )
           ],
         ),
       ),
